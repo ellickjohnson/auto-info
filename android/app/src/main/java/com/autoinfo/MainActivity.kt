da84b29c66896000de8379d5686c4c9820d0268c
Ellick Johnson <ellickjohnson@gmail.com>
2026-02-28 22:16:14 -0700

    Add/Update android/app/src/main/java/com/autoinfo/MainActivity.kt

---
+++ android/app/src/main/java/com/autoinfo/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.autoinfo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.autoinfo.ui.navigation.NavGraph
+import com.autoinfo.ui.theme.AutoInfoTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AutoInfoTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    NavGraph(navController = navController)
+                }
+            }
+        }
+    }
+}
